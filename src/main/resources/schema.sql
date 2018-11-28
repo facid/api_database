d17aa63e4f5ea5bf76ccf918c4f64c8e71f211ea
IonovaAK <ionova.alra@gmail.com>
2018-11-28 12:32:16 +0300

    Добавлена схема БД

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,72 @@
+CREATE TABLE IF NOT EXISTS Organization (
+    id          INTEGER  PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(10) NOT NULL,
+    full_name   VARCHAR(50) UNIQUE NOT NULL,
+    address     VARCHAR(50) NOT NULL,
+    phone       VARCHAR(50) UNIQUE,
+    inn         VARCHAR(50) UNIQUE NOT NULL,
+    kpp         VARCHAR(50) UNIQUE NOT NULL,
+    is_active   BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS Office (
+    id         INTEGER PRIMARY KEY AUTO_INCREMENT,
+    org_id     INTEGER NOT NULL,
+    name       VARCHAR(10) UNIQUE NOT NULL,
+    phone      VARCHAR(50) UNIQUE,
+    address    VARCHAR(50) NOT NULL,
+    is_active  BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS Employee (
+    id               INTEGER PRIMARY KEY AUTO_INCREMENT,
+    office_id        INTEGER NOT NULL,
+    first_name       VARCHAR(50) NOT NULL,
+    second_name      VARCHAR(50),
+    last_name        VARCHAR(50) NOT NULL,
+    position         VARCHAR(50) NOT NULL,
+    phone            VARCHAR(50) UNIQUE,
+    doc_code         VARCHAR(50),
+    doc_number       VARCHAR(50) UNIQUE,
+    doc_date         DATE,
+    citizenship_code INTEGER,
+    is_identified    BOOLEAN
+);
+
+CREATE TABLE IF NOT EXISTS Document (
+    id     INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name   VARCHAR(100),
+    code   VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS Citizenship (
+    id     INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name   VARCHAR(50),
+    code   INTEGER
+);
+
+CREATE INDEX UX_Organization_Full_Name ON Organization (full_name);
+CREATE INDEX UX_Organization_INN ON Organization (inn);
+CREATE INDEX UX_Organization_KPP ON Organization (kpp);
+CREATE INDEX IX_Organization_Is_Active ON Organization (is_active);
+
+CREATE INDEX UX_Office_Name ON Office (name);
+CREATE INDEX UX_Office_Phone ON Office (phone);
+CREATE INDEX IX_Office_Is_Active ON Office (is_active);
+
+CREATE INDEX IX_Employee_First_Name ON Employee (first_name);
+CREATE INDEX IX_Employee_Second_Name ON Employee (second_name);
+CREATE INDEX IX_Employee_Last_Name ON Employee (last_name);
+CREATE INDEX IX_Employee_Position ON Employee (position);
+
+CREATE INDEX IX_Office_Org_Id ON Office (org_id);
+ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization (id);
+
+CREATE INDEX IX_Employee_Office_Id ON Employee (office_id);
+ALTER TABLE Employee ADD FOREIGN KEY (office_id) REFERENCES Office (id);
+
+CREATE INDEX IX_Employee_Doc_Code ON Document (code);
+ALTER TABLE Employee ADD FOREIGN KEY (doc_code) REFERENCES Document (code);
+
+CREATE INDEX IX_Employee_Citizenship_Code ON Employee (citizenship_code);
+ALTER TABLE Employee ADD FOREIGN KEY (citizenship_code) REFERENCES Citizenship (code);
