cbed797ffb43e1ee805b2ea1d5cd80e05546af28
IonovaAK <ionova.alra@gmail.com>
2018-12-03 13:25:25 +0300

    Обновлена схема БД

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,9 +3,9 @@ CREATE TABLE IF NOT EXISTS Organization (
     name        VARCHAR(10) NOT NULL,
     full_name   VARCHAR(50) UNIQUE NOT NULL,
     address     VARCHAR(50) NOT NULL,
-    phone       VARCHAR(50) UNIQUE,
-    inn         VARCHAR(50) UNIQUE NOT NULL,
-    kpp         VARCHAR(50) UNIQUE NOT NULL,
+    phone       VARCHAR(15) UNIQUE,
+    inn         VARCHAR(10) UNIQUE NOT NULL,
+    kpp         VARCHAR(9) UNIQUE NOT NULL,
     is_active   BOOLEAN
 );
 
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS Office (
     id         INTEGER PRIMARY KEY AUTO_INCREMENT,
     org_id     INTEGER NOT NULL,
     name       VARCHAR(50) UNIQUE NOT NULL,
-    phone      VARCHAR(50) UNIQUE,
+    phone      VARCHAR(15) UNIQUE,
     address    VARCHAR(50) NOT NULL,
     is_active  BOOLEAN
 );
@@ -21,28 +21,33 @@ CREATE TABLE IF NOT EXISTS Office (
 CREATE TABLE IF NOT EXISTS Employee (
     id               INTEGER PRIMARY KEY AUTO_INCREMENT,
     office_id        INTEGER NOT NULL,
+    doc_data_id      INTEGER NOT NULL,
+    country_id       INTEGER NOT NULL,
     first_name       VARCHAR(50) NOT NULL,
     second_name      VARCHAR(50),
     last_name        VARCHAR(50),
     position         VARCHAR(50) NOT NULL,
-    phone            VARCHAR(50) UNIQUE,
-    doc_code         VARCHAR(50),
-    doc_number       VARCHAR(50) UNIQUE,
-    doc_date         DATE,
-    citizenship_code INTEGER,
+    phone            VARCHAR(15) UNIQUE,
     is_identified    BOOLEAN
 );
 
-CREATE TABLE IF NOT EXISTS DocumentType (
-    id     INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name   VARCHAR(100),
-    code   VARCHAR(50)
+CREATE TABLE IF NOT EXISTS Document_Type (
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(50),
+    code        VARCHAR(2)
+);
+
+CREATE TABLE IF NOT EXISTS Document_Data(
+    id              INTEGER PRIMARY KEY AUTO_INCREMENT,
+    doc_type_id     INTEGER NOT NULL,
+    doc_number      VARCHAR(10) UNIQUE,
+    doc_date        DATE
 );
 
 CREATE TABLE IF NOT EXISTS Country (
     id     INTEGER PRIMARY KEY AUTO_INCREMENT,
     name   VARCHAR(50),
-    code   INTEGER
+    code   VARCHAR(3)
 );
 
 CREATE INDEX UX_Organization_Full_Name ON Organization (full_name);
@@ -64,8 +69,11 @@ ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization (id);
 CREATE INDEX IX_Employee_Office_Id ON Employee (office_id);
 ALTER TABLE Employee ADD FOREIGN KEY (office_id) REFERENCES Office (id);
 
-CREATE INDEX IX_Employee_Doc_Code ON DocumentType (code);
-ALTER TABLE Employee ADD FOREIGN KEY (doc_code) REFERENCES DocumentType (code);
+CREATE INDEX IX_Document_Data_Doc_Type_Id ON Document_Data (doc_type_id);
+ALTER TABLE Document_Data ADD FOREING KEY (doc_type_id) REFERENCES Document_Type (id);
 
-CREATE INDEX IX_Employee_Citizenship_Code ON Employee (citizenship_code);
-ALTER TABLE Employee ADD FOREIGN KEY (citizenship_code) REFERENCES Country (code);
+CREATE INDEX IX_Employee_Doc_Data_Id ON Employee (doc_data_id);
+ALTER TABLE Employee ADD FOREIGN KEY (doc_data_id) REFERENCES Document_Data (id);
+
+CREATE INDEX IX_Employee_Citizenship_Code ON Employee (country_id);
+ALTER TABLE Employee ADD FOREIGN KEY (country_id) REFERENCES Country (id);
