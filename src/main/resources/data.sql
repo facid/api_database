b49ddeddec501679ef01c4d3f38a8c95cef074c8
IonovaAK <ionova.alra@gmail.com>
2019-01-10 02:03:11 +0300

    Обновлены resources

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,21 @@
-INSERT INTO Organization (id, version, name, full_name, address, phone, inn, kpp, is_active) VALUES (1, 0, 'JetBrains', 'JetBrains', 'Краснопресненская наб., 14, стр. 1', '', '1234567890', '123456789', TRUE);
+INSERT INTO Organization (id, version, name, full_name, address, phone, inn, kpp, is_active) VALUES (1, 0, 'JetBrains', 'JetBrains', 'Краснопресненская наб., 14, стр. 1', '849595951', '1234567890', '123456789', TRUE);
 
-INSERT INTO Organization (id, version, name, full_name, address, phone, inn, kpp, is_active) VALUES (2, 0, 'Сбербанк', 'ПАО Сбербанк', 'ул. Кутузовская, д.4', '', '1234567891', '123456788', TRUE);
+INSERT INTO Organization (id, version, name, full_name, address, phone, inn, kpp, is_active) VALUES (2, 0, 'Сбербанк', 'ПАО Сбербанк', 'ул. Кутузовская, д.4', '8800553531', '1234567891', '123456788', TRUE);
 
 INSERT INTO Organization (id, version, name, full_name, address, phone, inn, kpp, is_active) VALUES (3, 0, 'ФБК', 'Фонд борьбы с коррупцией', 'ул. Борьбы, д.13', '', '1234567892', '123456787', TRUE);
 
-INSERT INTO Organization (id, version, name, full_name, address, phone, inn, kpp, is_active) VALUES (4, 0, 'Аналитика', 'ОАО Аналитика', 'ул. Математической статистики, д.20', '', '1234567777', '123456777', FALSE);
+INSERT INTO Organization (id, version, name, full_name, address, phone, inn, kpp, is_active) VALUES (4, 0, 'Аналитика', 'ОАО Аналитика', 'ул. Математической статистики, д.20', '894562323', '1234567777', '123456777', FALSE);
 
 
-INSERT INTO Office (id, version, org_id, name, phone, address, is_active) VALUES (1, 0, 1, 'Деловой центр', '', 'Краснопресненская наб., 14, стр. 1', TRUE);
+INSERT INTO Office (id, version, org_id, name, phone, address, is_active) VALUES (1, 0, 1, 'Деловой центр', '849595951', 'Краснопресненская наб., 14, стр. 1', TRUE);
 
-INSERT INTO Office (id, version, org_id, name, phone, address, is_active) VALUES (2, 0, 2, 'Президент Плаза', '', 'ул. Кутузовская, д.4', TRUE);
+INSERT INTO Office (id, version, org_id, name, phone, address, is_active) VALUES (2, 0, 2, 'Президент Плаза', '8800553531', 'ул. Кутузовская, д.4', TRUE);
 
-INSERT INTO Office (id, version, org_id, name, phone, address, is_active) VALUES (3, 0, 2, 'Сбербанк плаза', '', 'ул. Грефа, д.8', TRUE);
+INSERT INTO Office (id, version, org_id, name, phone, address, is_active) VALUES (3, 0, 2, 'Сбербанк плаза', '849563225', 'ул. Грефа, д.8', TRUE);
 
 INSERT INTO Office (id, version, org_id, name, phone, address, is_active) VALUES (4, 0, 3, 'Статистика', '', 'ул. Борьбы, д.13', TRUE);
 
-INSERT INTO Office (id, version, org_id, name, phone, address, is_active) VALUES (5, 0, 4, 'Матстат', '', 'ул. Математической статистики, д.20', FALSE);
+INSERT INTO Office (id, version, org_id, name, phone, address, is_active) VALUES (5, 0, 4, 'Матстат', '894562323', 'ул. Математической статистики, д.20', FALSE);
 
 
 INSERT INTO Document_Type (id, version, name, code) VALUES (1, 0, 'Паспорт гражданина РФ', '21');
@@ -38,19 +38,19 @@ INSERT INTO Document_Data (id, version, doc_type_id, doc_number, doc_date) VALUE
 INSERT INTO Document_Data (id, version, doc_type_id, doc_number, doc_date) VALUES (6, 0, 2, '826499121', '1996-01-28');
 
 
-INSERT INTO Country (id, version, name, code) VALUES (1, 0, 'Российская Федерация', '630');
+INSERT INTO Country (id, version, name, code) VALUES (1, 0, 'Российская Федерация', '643');
 
 INSERT INTO Country (id, version, name, code) VALUES (2, 0, 'Республика Беларусь', '112');
 
 
-INSERT INTO Employee (id, version, office_id, doc_data_id, country_id, first_name, second_name, last_name, position, phone, is_identified) VALUES (1, 0, 1, 1, 1, 'Владимир', 'Иванович', 'Иванов', 'Программист', '89685061821', TRUE);
+INSERT INTO Employee (id, version, office_id, doc_data_id, country_id, first_name, middle_name, second_name, position, phone, is_identified) VALUES (1, 0, 1, 1, 1, 'Владимир', 'Иванович', 'Иванов', 'Программист', '89685061821', TRUE);
 
-INSERT INTO Employee (id, version, office_id, doc_data_id, country_id, first_name, second_name, last_name, position, phone, is_identified) VALUES (2, 0, 2, 2, 1, 'Владислав', '', 'Перевощиков', 'Аналитик', '', TRUE);
+INSERT INTO Employee (id, version, office_id, doc_data_id, country_id, first_name, middle_name, second_name, position, phone, is_identified) VALUES (2, 0, 2, 2, 1, 'Владислав', '', 'Перевощиков', 'Аналитик', '', TRUE);
 
-INSERT INTO Employee (id, version, office_id, doc_data_id, country_id, first_name, second_name, last_name, position, phone, is_identified) VALUES (3, 0, 2, 3, 1, 'Александра', '', 'Иванова', 'Программист', '89675061420', TRUE);
+INSERT INTO Employee (id, version, office_id, doc_data_id, country_id, first_name, middle_name, second_name, position, phone, is_identified) VALUES (3, 0, 2, 3, 1, 'Александра', '', 'Иванова', 'Программист', '89675061420', TRUE);
 
-INSERT INTO Employee (id, version, office_id, doc_data_id, country_id, first_name, second_name, last_name, position, phone, is_identified) VALUES (4, 0, 3, 4, 1, 'Алина', '', 'Александрова', 'Менеджер', '89051012021', TRUE);
+INSERT INTO Employee (id, version, office_id, doc_data_id, country_id, first_name, middle_name, second_name, position, phone, is_identified) VALUES (4, 0, 3, 4, 1, 'Алина', '', 'Александрова', 'Менеджер', '89051012021', TRUE);
 
-INSERT INTO Employee (id, version, office_id, doc_data_id, country_id, first_name, second_name, last_name, position, phone, is_identified) VALUES (5, 0, 4, 5, 1, 'Владислав', 'Владимирович', 'Осютин', 'Секретарь', '', TRUE);
+INSERT INTO Employee (id, version, office_id, doc_data_id, country_id, first_name, middle_name, second_name, position, phone, is_identified) VALUES (5, 0, 4, 5, 1, 'Владислав', 'Владимирович', 'Осютин', 'Секретарь', '', TRUE);
 
-INSERT INTO Employee (id, version, office_id, doc_data_id, country_id, first_name, second_name, last_name, position, phone, is_identified) VALUES (6, 0, 1, 6, 2, 'Захар', '', 'Мазуров', 'Программист', '89456221', TRUE);
+INSERT INTO Employee (id, version, office_id, doc_data_id, country_id, first_name, middle_name, second_name, position, phone, is_identified) VALUES (6, 0, 1, 6, 2, 'Захар', '', 'Мазуров', 'Программист', '89456221', TRUE);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,78 +1,78 @@
 CREATE TABLE IF NOT EXISTS Organization (
-    id          INTEGER PRIMARY KEY AUTO_INCREMENT ,
-    version     INTEGER NOT NULL,
-    name        VARCHAR(10) NOT NULL,
-    full_name   VARCHAR(50) NOT NULL,
-    address     VARCHAR(50) NOT NULL,
-    phone       VARCHAR(15),
-    inn         VARCHAR(10) NOT NULL,
-    kpp         VARCHAR(9) NOT NULL,
-    is_active   BOOLEAN
+    id          INTEGER                 COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version     INTEGER NOT NULL        COMMENT 'Служебное поле hibernate',
+    name        VARCHAR(10) NOT NULL    COMMENT 'Название',
+    full_name   VARCHAR(50) NOT NULL    COMMENT 'Полное название',
+    address     VARCHAR(50) NOT NULL    COMMENT 'Адрес',
+    phone       VARCHAR                 COMMENT 'Телефон',
+    inn         VARCHAR(10) NOT NULL    COMMENT 'ИНН',
+    kpp         VARCHAR(9) NOT NULL     COMMENT 'КПП',
+    is_active   BOOLEAN                 COMMENT 'Активность'
 );
 COMMENT ON TABLE Organization IS 'Организация';
 
 CREATE TABLE IF NOT EXISTS Office (
-    id         INTEGER PRIMARY KEY AUTO_INCREMENT ,
-    version    INTEGER NOT NULL,
-    org_id     INTEGER NOT NULL,
-    name       VARCHAR(50) NOT NULL,
-    phone      VARCHAR(15),
-    address    VARCHAR(50) NOT NULL,
-    is_active  BOOLEAN
+    id         INTEGER                  COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version    INTEGER NOT NULL         COMMENT 'Служебное поле hibernate',
+    org_id     INTEGER                  COMMENT 'Уникальный идентификатор организации',
+    name       VARCHAR(50)              COMMENT 'Название',
+    phone      VARCHAR(15)              COMMENT 'Телефон',
+    address    VARCHAR(50)              COMMENT 'Адрес',
+    is_active  BOOLEAN                  COMMENT 'Активность'
 );
 COMMENT ON TABLE Office IS 'Офис';
 
 CREATE TABLE IF NOT EXISTS Employee (
-    id               INTEGER PRIMARY KEY AUTO_INCREMENT ,
-    version          INTEGER NOT NULL,
-    office_id        INTEGER NOT NULL,
-    doc_data_id      INTEGER NOT NULL,
-    country_id       INTEGER NOT NULL,
-    first_name       VARCHAR(50) NOT NULL,
-    second_name      VARCHAR(50),
-    last_name        VARCHAR(50),
-    position         VARCHAR(50) NOT NULL,
-    phone            VARCHAR(15),
-    is_identified    BOOLEAN
+    id               INTEGER                COMMENT 'Уникальный идетификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version          INTEGER NOT NULL       COMMENT 'Служебное поле hibernate',
+    office_id        INTEGER                COMMENT 'Уникальный идентификатор офиса',
+    doc_data_id      INTEGER                COMMENT 'Уникальный идентификатор данных документа',
+    country_id       INTEGER                COMMENT 'Уникальный идентификатор гражданства',
+    first_name       VARCHAR(50) NOT NULL   COMMENT 'Имя',
+    middle_name      VARCHAR(50)            COMMENT 'Отчество',
+    second_name      VARCHAR(50)            COMMENT 'Фамилия',
+    position         VARCHAR(50) NOT NULL   COMMENT 'Должность',
+    phone            VARCHAR(15)            COMMENT 'Телефон',
+    is_identified    BOOLEAN                COMMENT 'Идентификация'
 );
 COMMENT ON TABLE Employee IS 'Работник';
 
 CREATE TABLE IF NOT EXISTS Document_Type (
-    id          INTEGER PRIMARY KEY AUTO_INCREMENT ,
-    version     INTEGER NOT NULL,
-    name        VARCHAR(50),
-    code        VARCHAR(2)
+    id          INTEGER             COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version     INTEGER NOT NULL    COMMENT 'Служебное поле hibernate',
+    name        VARCHAR(50)         COMMENT 'Название',
+    code        VARCHAR(2)          COMMENT 'Код'
 );
 COMMENT ON TABLE Document_Type IS 'Тип документа';
 
 CREATE TABLE IF NOT EXISTS Document_Data(
-    id              INTEGER PRIMARY KEY AUTO_INCREMENT ,
-    version         INTEGER NOT NULL,
-    doc_type_id     INTEGER NOT NULL,
-    doc_number      VARCHAR(10) NOT NULL,
-    doc_date        DATE
+    id              INTEGER            COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version         INTEGER NOT NULL   COMMENT 'Служебное поле hibernate',
+    doc_type_id     INTEGER            COMMENT 'Уникальный идентификатор данных документа',
+    doc_number      VARCHAR(10)        COMMENT 'Номер',
+    doc_date        DATE               COMMENT 'Дата'
 );
 COMMENT ON TABLE Document_Data IS 'Данные документа';
 
 CREATE TABLE IF NOT EXISTS Country (
-    id        INTEGER PRIMARY KEY AUTO_INCREMENT ,
-    version   INTEGER NOT NULL,
-    name      VARCHAR(50),
-    code      VARCHAR(3)
+    id        INTEGER           COMMENT 'Уникальный идентификатор' PRIMARY KEY AUTO_INCREMENT ,
+    version   INTEGER NOT NULL  COMMENT 'Служебное поле hibernate',
+    name      VARCHAR(50)       COMMENT 'Название',
+    code      VARCHAR(3)        COMMENT 'Код'
 );
-COMMENT ON TABLE Country IS 'Страна';
+COMMENT ON TABLE Country IS 'Гражданство';
 
-CREATE INDEX UX_Organization_Full_Name ON Organization (full_name);
-CREATE INDEX UX_Organization_INN ON Organization (inn);
+CREATE INDEX IX_Organization_Full_Name ON Organization (full_name);
+CREATE INDEX IX_Organization_INN ON Organization (inn);
 CREATE INDEX IX_Organization_Is_Active ON Organization (is_active);
 
-CREATE INDEX UX_Office_Name ON Office (name);
-CREATE INDEX UX_Office_Phone ON Office (phone);
+CREATE INDEX IX_Office_Name ON Office (name);
+CREATE INDEX IX_Office_Phone ON Office (phone);
 CREATE INDEX IX_Office_Is_Active ON Office (is_active);
 
 CREATE INDEX IX_Employee_First_Name ON Employee (first_name);
+CREATE INDEX IX_Employee_Last_Name ON Employee (middle_name);
 CREATE INDEX IX_Employee_Second_Name ON Employee (second_name);
-CREATE INDEX IX_Employee_Last_Name ON Employee (last_name);
 CREATE INDEX IX_Employee_Position ON Employee (position);
 
 CREATE INDEX IX_Office_Org_Id ON Office (org_id);
